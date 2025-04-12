3f58a91b312a015f4eb42cfa9704d6304355208b
dady <112051772+Jakee8718@users.noreply.github.com>
2025-04-12 13:54:51 -0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,18 +1,4 @@
--- Global table to track ESP objects
-local drawings = {}
-
--- Cleanup function to remove all ESP elements
-getgenv().espCleanup = function()
-    for _, obj in pairs(drawings) do
-        if obj.Remove then
-            obj:Remove()
-        end
-    end
-    drawings = {} -- Reset the drawing table
-    print("ESP Cleaned up!")
-end
-
-local ESP_ENABLED = false -- Default is off
+local ESP_ENABLED = true -- Toggle ESP on/off
 local NAMETAGS_ENABLED = false -- Toggle NameTags on/off with key
 local COLOR = Color3.fromRGB(255, 0, 0) -- Box color
 local LINE_THICKNESS = 2
@@ -31,17 +17,14 @@ UserInputService.InputBegan:Connect(function(input, gameProcessed)
     end
 end)
 
--- Create ESP Box for a player
 local function CreateESPBox(character, player)
     local Box = Drawing.new("Square")
-    table.insert(drawings, Box)  -- Track the box for cleanup
     Box.Visible = false
     Box.Color = COLOR
     Box.Thickness = LINE_THICKNESS
     Box.Filled = false
 
     local NameTag = Drawing.new("Text")
-    table.insert(drawings, NameTag)  -- Track the NameTag for cleanup
     NameTag.Visible = false
     NameTag.Color = COLOR
     NameTag.Size = 16
@@ -55,10 +38,12 @@ local function CreateESPBox(character, player)
                 if HumanoidRootPart then
                     local Vector, OnScreen = Camera:WorldToViewportPoint(HumanoidRootPart.Position)
                     if OnScreen then
+                        -- ESP Box
                         Box.Size = Vector2.new(50, 100)
                         Box.Position = Vector2.new(Vector.X - 25, Vector.Y - 50)
                         Box.Visible = true
 
+                        -- NameTag
                         if NAMETAGS_ENABLED then
                             local distance = math.floor((HumanoidRootPart.Position - Camera.CFrame.Position).Magnitude)
                             NameTag.Text = string.format("%s (@%s)\n[%d studs]", player.DisplayName, player.Name, distance)
@@ -88,18 +73,13 @@ local function CreateESPBox(character, player)
     coroutine.wrap(Update)()
 end
 
--- Player added event to add ESP to new players
 local function OnPlayerAdded(player)
     if player ~= LocalPlayer then
         player.CharacterAdded:Connect(function(character)
-            if ESP_ENABLED then
-                CreateESPBox(character, player)
-            end
+            CreateESPBox(character, player)
         end)
         if player.Character then
-            if ESP_ENABLED then
-                CreateESPBox(player.Character, player)
-            end
+            CreateESPBox(player.Character, player)
         end
     end
 end
