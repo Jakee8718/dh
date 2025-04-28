377eaa442563b317200d5169aa907fe5933f8652
dady <112051772+Jakee8718@users.noreply.github.com>
2025-04-27 20:55:11 -0400

    Update teleports.lua

--- teleports.lua
+++ teleports.lua
@@ -2,19 +2,17 @@
 
 local Teleports = {}
 
--- 1) Locations
+-- 1) Locations (note decimal points and quoted key)
 Teleports.locations = {
-    Revolver = CFrame.new(-636.762, 21.748, -125,627),
-    Bank     = CFrame.new(-405.471, 21.748, -284,517),
-    Military = CFrame.new(38.660, 25.253, -869.897),
-    LMG = CFrame.new(-625.019, 23.244, -298.706),
-    Downhill Food = CFrame.new(-331,280, 23.681, -298.417),
-    
-    -- etc…
+    Revolver         = CFrame.new(-636.762, 21.748, -125.627),
+    Bank             = CFrame.new(-405.471, 21.748, -284.517),
+    Military         = CFrame.new(  38.660, 25.253, -869.897),
+    LMG              = CFrame.new(-625.019, 23.244, -298.706),
+    ["Downhill Food"] = CFrame.new(-331.280, 23.681, -298.417),
+    -- add more here…
 }
 
-
--- 3) Safe teleport function
+-- 2) Safe teleport function
 function Teleports.teleport(name)
     local cf = Teleports.locations[name]
     if not cf then return end
