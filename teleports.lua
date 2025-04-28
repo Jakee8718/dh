5f5c6e83d17d47ea1daf1191017b981c99e48075
dady <112051772+Jakee8718@users.noreply.github.com>
2025-04-27 20:02:56 -0400

    Update teleports.lua

--- teleports.lua
+++ teleports.lua
@@ -2,15 +2,23 @@
 
 local Teleports = {}
 
--- 1) List all your spots here (just add more entries as you go)
+-- 1) Locations
 Teleports.locations = {
-    Revolver    = CFrame.new(-648.365356, 22.4022942, -119.864662),
-    Bank        = CFrame.new(100,       22,         -50),
-    TacoShop    = CFrame.new(300,       20,         -100),
+    Revolver = CFrame.new(-648.365356, 22.4022942, -119.864662),
+    Bank     = CFrame.new(100,       22,         -50),
+    TacoShop = CFrame.new(300,       20,         -100),
     -- etc…
 }
 
--- 2) Encapsulated “safe teleport” function
+-- 2) Icons
+Teleports.icons = {
+    Revolver = "rbxassetid://96808607551383",
+    Bank     = "rbxassetid://12345678901234",  -- replace with your Bank icon ID
+    TacoShop = "rbxassetid://23456789012345",  -- replace with your Taco Shop icon ID
+    -- etc…
+}
+
+-- 3) Safe teleport function
 function Teleports.teleport(name)
     local cf = Teleports.locations[name]
     if not cf then return end
